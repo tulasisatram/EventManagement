48033a2b6742ba326d419f2e08c924cc146c5aaf
tulasi <tulasipinjala.com>
2019-01-30 10:13:13 -0500

    initial

---
+++ src/main/java/eventmanagement/EventManagement.java
@@ -0,0 +1,10 @@
+package eventmanagement;
+
+public class EventManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/eventmanagement/EventManagementTest.java
@@ -0,0 +1,10 @@
+package eventmanagement;
+
+public class EventManagementTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
